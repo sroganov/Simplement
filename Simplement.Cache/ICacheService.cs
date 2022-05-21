1a3f39302acd6750d8073953bb46bbb84eec802e
unix3dgforce <unix3dgforce@gmail.com>
2022-05-21 18:41:57 +0500

    Add Async method to Cache

--- Simplement.Cache/ICacheService.cs
+++ Simplement.Cache/ICacheService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Simplement.Core;
 
 namespace Simplement.Cache
@@ -14,10 +15,21 @@ namespace Simplement.Cache
         OperationResult<T> Get<T>(string key);
 
         /// <summary>
+        /// Async Gets existing value or null if no value exists.
+        /// </summary>
+        Task<OperationResult<T>> GetAsync<T>(string key);
+
+        /// <summary>
         /// Gets existing value or adds new one using creator function if no value exists.
         /// Cache expiration timeout in minutes.
         /// </summary>
         OperationResult<T> Get<T>(string key, Func<T> creator, int expiration = 1440);
+
+        /// <summary>
+        /// Async Gets existing value or adds new one using creator function if no value exists.
+        /// Cache expiration timeout in minutes.
+        /// </summary>
+        Task<OperationResult<T>> GetAsync<T>(string key, Func<Task<T>> creator, int expiration = 1440);
 
         /// <summary>
         /// Adds new value into cache if no value exists for given key, otherwise fails.
@@ -26,10 +38,22 @@ namespace Simplement.Cache
         OperationResult<T> Add<T>(string key, T value, int expiration = 1440);
 
         /// <summary>
+        /// Async Adds new value into cache if no value exists for given key, otherwise fails.
+        /// Cache expiration timeout in minutes.
+        /// </summary>
+        Task<OperationResult<T>> AddAsync<T>(string key, T value, int expiration = 1440);
+
+        /// <summary>
         /// Updates existing value.
         /// Cache expiration timeout in minutes.
         /// </summary>
         OperationResult Update<T>(string key, T value, int expiration = 1440);
+
+        /// <summary>
+        /// Async Updates existing value.
+        /// Cache expiration timeout in minutes.
+        /// </summary>
+        Task<OperationResult> UpdateAsync<T>(string key, T value, int expiration = 1440);
 
         /// <summary>
         /// Removes existing value.
@@ -37,8 +61,18 @@ namespace Simplement.Cache
         OperationResult Delete(string key);
 
         /// <summary>
+        /// Async Removes existing value.
+        /// </summary>
+        Task<OperationResult> DeleteAsync(string key);
+
+        /// <summary>
         /// Removes ALL values in cache storage.
         /// </summary>
         OperationResult Clear();
+
+        /// <summary>
+        /// Async Removes ALL values in cache storage.
+        /// </summary>
+        Task<OperationResult> ClearAsync();
     }
 }
--- Simplement.Cache/MemoryCacheService.cs
+++ Simplement.Cache/MemoryCacheService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Runtime.Caching;
+using System.Threading.Tasks;
 using Simplement.Core;
 
 namespace Simplement.Cache
@@ -11,14 +12,14 @@ namespace Simplement.Cache
         private const string KeyPrefix = "memory_cache_service";
         private static string GetCacheKey(string key) => $"{KeyPrefix}_{key}";
 
-        private static ObjectCache Storage => MemoryCache.Default;
+        protected ObjectCache Storage => MemoryCache.Default;
 
         public OperationResult<T> Get<T>(string key)
         {
             return Get<T>(key, null);
         }
 
-        public OperationResult<T> Get<T>(string key, Func<T> creator, int expiration = 1440)
+        public virtual OperationResult<T> Get<T>(string key, Func<T> creator, int expiration = 1440)
         {
             var fullKey = GetCacheKey(key);
             if (Storage.Contains(fullKey))
@@ -42,7 +43,7 @@ namespace Simplement.Cache
             };
         }
 
-        public OperationResult<T> Add<T>(string key, T value, int expiration = 1440)
+        public virtual OperationResult<T> Add<T>(string key, T value, int expiration = 1440)
         {
             var fullKey = GetCacheKey(key);
             if (Storage.Contains(fullKey))
@@ -58,7 +59,7 @@ namespace Simplement.Cache
             };
         }
 
-        public OperationResult Update<T>(string key, T value, int expiration = 1440)
+        public virtual OperationResult Update<T>(string key, T value, int expiration = 1440)
         {
             if (value == null)
                 return OperationResult.Fail();
@@ -71,7 +72,7 @@ namespace Simplement.Cache
             return OperationResult.Success();
         }
 
-        public OperationResult Delete(string key)
+        public virtual OperationResult Delete(string key)
         {
             var fullKey = GetCacheKey(key);
             if (!Storage.Contains(fullKey))
@@ -81,7 +82,7 @@ namespace Simplement.Cache
             return OperationResult.Success();
         }
 
-        public OperationResult Clear()
+        public virtual OperationResult Clear()
         {
             var items = Storage.Where(i => i.Key.StartsWith(KeyPrefix)).ToList();
             foreach (var item in items)
@@ -90,12 +91,69 @@ namespace Simplement.Cache
             return OperationResult.Success();
         }
 
-        private static T GetValue<T>(Func<T> creator)
+        protected static T GetValue<T>(Func<T> creator)
         {
             if (creator == null)
                 return default;
 
             return creator() ?? default;
+        }
+
+        protected static async Task<T> GetValueAsync<T>(Func<Task<T>> creator)
+        {
+            if (creator == null)
+                return default;
+
+            return await creator() ?? default;
+        }
+
+        public async Task<OperationResult<T>> GetAsync<T>(string key)
+        {
+            return Get<T>(key);
+        }
+
+        public async Task<OperationResult<T>> GetAsync<T>(string key, Func<Task<T>> creator, int expiration = 1440)
+        {
+            var fullKey = GetCacheKey(key);
+            if (Storage.Contains(fullKey))
+                return new OperationResult<T>
+                {
+                    Result = OperationStatus.Success,
+                    Value = (T) Storage.Get(fullKey)
+                };
+
+            var value = await GetValueAsync(creator);
+            if (value == null)
+                return OperationResult<T>.Fail();
+
+            if (!Storage.Add(fullKey, value, DateTime.Now.AddMinutes(expiration)))
+                return OperationResult<T>.Fail();
+
+            return new OperationResult<T>
+            {
+                Result = OperationStatus.Success,
+                Value = value
+            };
+        }
+
+        public async Task<OperationResult<T>> AddAsync<T>(string key, T value, int expiration = 1440)
+        {
+            return Add<T>(key, value, expiration);
+        }
+
+        public async Task<OperationResult> UpdateAsync<T>(string key, T value, int expiration = 1440)
+        {
+            return Update<T>(key, value, expiration);
+        }
+
+        public async Task<OperationResult> DeleteAsync(string key)
+        {
+            return Delete(key);
+        }
+
+        public async Task<OperationResult> ClearAsync()
+        {
+            return Clear();
         }
     }
 }
